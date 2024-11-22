507b8b34c447e72e7a736ceb729ac879c623aabf
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2024-11-22 09:08:45 +0700

    fix: PHONE_CONFLICT and EMAIL_CONFLICT

--- src/modules/admin/merchants/merchants.controller.ts
+++ src/modules/admin/merchants/merchants.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, NotFoundException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
 import { MerchantsService } from './merchants.service';
 import { CreateMerchantDto } from './dto/create-merchant.dto';
 import { UpdateMerchantDto } from './dto/update-merchant.dto';
@@ -6,7 +17,8 @@ import { ApiTags } from '@nestjs/swagger';
 import { MerchantEntity } from 'src/database/entities/merchant.entity';
 import { hashPassword } from 'src/utils/bcrypt';
 import { QueryMerchantDto } from './dto/query-merchant.dto';
-import { FindManyOptions, FindOptionsWhere, ILike } from 'typeorm';
+import { FindManyOptions, FindOptionsWhere, ILike, Not } from 'typeorm';
+import { ADMIN_EXCEPTIONS } from 'src/common/constants/admin.constant';
 
 @Controller('merchants')
 @ApiTags('Admin Merchants')
@@ -14,8 +26,14 @@ export class MerchantsController {
   constructor(private readonly merchantsService: MerchantsService) {}
 
   @Post()
-  create(@Body() createMerchantDto: CreateMerchantDto) {
-    const { password, email } = createMerchantDto;
+  async create(@Body() createMerchantDto: CreateMerchantDto) {
+    const { password, email, phone } = createMerchantDto;
+
+    const options = { where: [{ email }, { phone: createMerchantDto.phone }] };
+    const merchant = await this.merchantsService.findOne(options);
+
+    if (email && merchant?.email === email) throw new ConflictException(ADMIN_EXCEPTIONS.EMAIL_CONFLICT);
+    if (phone && merchant?.phone === phone) throw new ConflictException(ADMIN_EXCEPTIONS.PHONE_CONFLICT);
 
     const newMerchant = new MerchantEntity();
     Object.assign(newMerchant, createMerchantDto);
@@ -55,11 +73,17 @@ export class MerchantsController {
 
   @Patch(':id')
   async update(@Param('id') id: number, @Body() updateMerchantDto: UpdateMerchantDto) {
-    const { password, email } = updateMerchantDto;
+    const { password, email, phone } = updateMerchantDto;
 
     const options = { where: { id: +id } };
     const merchant = await this.merchantsService.findOne(options);
     if (!merchant) throw new NotFoundException();
+
+    const emailConflict = await this.merchantsService.findOne({ where: { email, id: Not(merchant.id) } });
+    if (emailConflict) throw new ConflictException(ADMIN_EXCEPTIONS.EMAIL_CONFLICT);
+
+    const phoneConflict = await this.merchantsService.findOne({ where: { phone, id: Not(merchant.id) } });
+    if (phoneConflict) throw new ConflictException(ADMIN_EXCEPTIONS.PHONE_CONFLICT);
 
     Object.assign(merchant, updateMerchantDto);
     password && (merchant.password = hashPassword(password));
