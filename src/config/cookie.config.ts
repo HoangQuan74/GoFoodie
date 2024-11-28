a35eaed4fe47228581f122417ada62b684ef9ad1
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2024-11-28 23:25:48 +0700

    fix cookie

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -5,6 +5,6 @@ const isProduction = ENV === 'production';
 export const cookieConfig: CookieOptions = {
   httpOnly: true,
   secure: isProduction,
-  sameSite: !isProduction ? 'none' : undefined,
+  sameSite: isProduction ? 'none' : 'lax',
   maxAge: 1000 * 60 * 60 * 24 * 7,
 };
