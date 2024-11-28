49e22302cf8adeb18ce7e4797933abeb1061d147
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2024-11-28 23:33:55 +0700

    fix cookie

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -5,6 +5,6 @@ const isProduction = ENV === 'production';
 export const cookieConfig: CookieOptions = {
   httpOnly: true,
   secure: isProduction,
-  sameSite: isProduction ? 'none' : 'lax',
+  sameSite: 'none',
   maxAge: 1000 * 60 * 60 * 24 * 7,
 };
--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsConfig: CorsOptions = {
-  origin: '*',
+  origin: ['http://localhost:8889', 'https://admin-dev.oneship.com.vn'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 };
