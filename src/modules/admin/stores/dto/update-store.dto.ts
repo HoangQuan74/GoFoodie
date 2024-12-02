35a8bf64c33ad0990e7ac98cdbc30f98c1bc695a
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2024-12-02 16:55:55 +0700

    fix: update store

--- src/modules/admin/stores/dto/update-store.dto.ts
+++ src/modules/admin/stores/dto/update-store.dto.ts
@@ -1,4 +1,18 @@
-import {  PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateStoreDto } from './create-store.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class UpdateStoreDto extends PartialType(CreateStoreDto) {}
+export class UpdateStoreDto extends PartialType(CreateStoreDto) {
+  @ApiPropertyOptional()
+  @IsBoolean()
+  @Type(() => Boolean)
+  @IsOptional()
+  isPause: boolean;
+
+  @ApiPropertyOptional()
+  @IsBoolean()
+  @Type(() => Boolean)
+  @IsOptional()
+  isSpecialWorkingTime: boolean;
+}
