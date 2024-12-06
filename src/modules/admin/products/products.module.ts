4ffe0b593433518168478e65f0c33b6649a681cd
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2024-12-06 20:18:56 +0700

    fix: build

--- src/modules/admin/products/products.module.ts
+++ src/modules/admin/products/products.module.ts
@@ -4,9 +4,10 @@ import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from 'src/database/entities/product.entity';
 import { StoresModule } from '../stores/stores.module';
+import { OptionGroupsModule } from '../option-groups/option-groups.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProductEntity]), StoresModule],
+  imports: [TypeOrmModule.forFeature([ProductEntity]), StoresModule, OptionGroupsModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
