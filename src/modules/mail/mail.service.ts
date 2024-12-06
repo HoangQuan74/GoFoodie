2fec9372496dd07b18b8ce93236011832e3709df
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2024-12-06 14:06:35 +0700

    fix: mail body

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -9,12 +9,12 @@ export class MailService {
   async sendForgotPasswordForAdmin(email: string, otp: string, name: string) {
     try {
       const logo = BASE_URL + '/public/logo.png';
-      const headerImage = BASE_URL + '/public/header-image.png';
+      const headerBg = BASE_URL + '/public/header-bg.png';
       return await this.mailerService.sendMail({
         to: email,
         subject: 'Forgot Password',
         template: 'forgot-password',
-        context: { otp, email, logo, name, headerImage },
+        context: { otp, email, logo, name, headerBg },
       });
     } catch (message) {
       return console.error(message);
