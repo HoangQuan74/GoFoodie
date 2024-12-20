fc76aaeb580fd954ef975ee44b440695e712d555
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2024-12-20 19:20:20 +0700

    fix: login sms

--- src/modules/merchant/auth/auth.service.ts
+++ src/modules/merchant/auth/auth.service.ts
@@ -79,7 +79,12 @@ export class AuthService {
         .setParameter('approvalStatus', EStoreApprovalStatus.Approved)
         .getOne();
 
-      if (!merchant) throw new UnauthorizedException(EXCEPTIONS.NOT_FOUND);
+      if (!merchant) {
+        merchant = new MerchantEntity();
+        merchant.phone = phone;
+        merchant = await this.merchantsService.save(merchant);
+      }
+
       if (merchant.status !== EMerchantStatus.Active) throw new UnauthorizedException(EXCEPTIONS.ACCOUNT_NOT_ACTIVE);
 
       const stores = merchant.store ? [merchant.store, ...merchant.stores] : merchant.stores;
