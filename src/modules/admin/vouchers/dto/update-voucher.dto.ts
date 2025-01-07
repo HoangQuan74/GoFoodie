5ca8c3d4ff793dbfadbfa4741010f45373976b70
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2025-01-07 16:38:41 +0700

    fix: update voucher

--- src/modules/admin/vouchers/dto/update-voucher.dto.ts
+++ src/modules/admin/vouchers/dto/update-voucher.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType, OmitType } from '@nestjs/swagger';
+import { PartialType, OmitType, ApiPropertyOptional } from '@nestjs/swagger';
 import { CreateVoucherDto } from './create-voucher.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateVoucherDto extends PartialType(OmitType(CreateVoucherDto, ['code', 'typeId'] as const)) {}
+export class UpdateVoucherDto extends PartialType(OmitType(CreateVoucherDto, ['code', 'typeId'] as const)) {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+}
