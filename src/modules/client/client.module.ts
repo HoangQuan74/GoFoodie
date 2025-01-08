7e54aa4635396de555f8bfb7afe89846d2337859
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2025-01-08 08:54:01 +0700

    fix: path client

--- src/modules/client/client.module.ts
+++ src/modules/client/client.module.ts
@@ -6,13 +6,19 @@ import { RouterModule } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientEntity } from 'src/database/entities/client.entity';
+import { StoresModule } from './stores/stores.module';
 
 @Module({
   imports: [
     BannersModule,
-    RouterModule.register([{ path: 'client', module: BannersModule }]),
+    RouterModule.register([
+      { path: 'client', module: BannersModule },
+      { path: 'client', module: StoresModule },
+      { path: 'client', module: AuthModule },
+    ]),
     AuthModule,
     TypeOrmModule.forFeature([ClientEntity]),
+    StoresModule,
   ],
   controllers: [ClientController],
   providers: [ClientService],
