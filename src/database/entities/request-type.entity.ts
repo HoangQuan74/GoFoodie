4b77a68869f20d6c7ee453b516a970b3ac4063fc
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2025-01-23 10:44:08 +0700

    feat: CRUD request type

--- src/database/entities/request-type.entity.ts
+++ src/database/entities/request-type.entity.ts
@@ -1,11 +1,31 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { BaseEntity } from './base.entity';
+import { AdminEntity } from './admin.entity';
+import { AppTypeEntity } from './app-type.entity';
+import { EAppType } from 'src/common/enums/config.enum';
 
 @Entity('request_types')
-export class RequestTypeEntity {
+export class RequestTypeEntity extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   name: string;
+
+  @Column({ name: 'is_active', default: true })
+  isActive: boolean;
+
+  @Column({ name: 'app_type_id', default: EAppType.AppClient })
+  appTypeId: EAppType;
+
+  @Column({ name: 'created_by_id', nullable: true })
+  createdById: number;
+
+  @ManyToOne(() => AdminEntity, (admin) => admin.id)
+  @JoinColumn({ name: 'created_by_id' })
+  createdBy: AdminEntity;
+
+  @ManyToOne(() => AppTypeEntity, (appType) => appType.value)
+  @JoinColumn({ name: 'app_type_id' })
+  appType: AppTypeEntity;
 }
