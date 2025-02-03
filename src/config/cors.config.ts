fef70b485147ffc7ceb35201c38a8d7503327d55
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2025-02-03 13:04:33 +0700

    fix: cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,7 +1,12 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsConfig: CorsOptions = {
-  origin: ['http://localhost:8889', 'https://admin-dev.oneship.com.vn', 'https://localhost:8890'],
+  origin: [
+    'http://localhost:8889',
+    'https://admin-dev.oneship.com.vn',
+    'https://admin-stag.oneship.com.vn',
+    'https://admin.oneship.com.vn',
+  ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 };
