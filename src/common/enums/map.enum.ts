16ff8b6be80dc5eb03f0d848164cb9753e561df5
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2025-02-06 16:52:41 +0700

    feat: function getDistanceAndDuration

---
+++ src/common/enums/map.enum.ts
@@ -0,0 +1,6 @@
+export enum EVehicleType {
+  Car = 'car',
+  Bike = 'bike',
+  Truck = 'truck',
+  Taxi = 'taxi',
+}
--- src/modules/mapbox/mapbox.service.ts
+++ src/modules/mapbox/mapbox.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { GOONG_API_KEY, GOONG_URL } from 'src/common/constants';
+import { EVehicleType } from 'src/common/enums/map.enum';
+import { IDistanceAndDuration, ILocation } from 'src/common/interfaces/map.interface';
 
 @Injectable()
-export class MapboxService {}
+export class MapboxService {
+  constructor() {}
+
+  async getDistanceAndDuration(
+    origin: ILocation,
+    destination: ILocation[],
+    vehicle: EVehicleType = EVehicleType.Car,
+  ): Promise<IDistanceAndDuration[]> {
+    const { data } = await axios.get(`${GOONG_URL}/DistanceMatrix`, {
+      params: {
+        api_key: GOONG_API_KEY,
+        origins: `${origin.lat},${origin.lng}`,
+        destinations: destination.map((loc) => `${loc.lat},${loc.lng}`).join('|'),
+        vehicle,
+      },
+    });
+
+    return data.rows[0].elements.map((element) => ({
+      distance: element.distance.value,
+      duration: element.duration.value,
+    }));
+  }
+}
