155003a6904c5558db8d52c808b2d9616748fca3
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2025-02-12 17:28:48 +0700

    fix: create card

--- src/common/constants/common.constant.ts
+++ src/common/constants/common.constant.ts
@@ -1,6 +1,6 @@
 export const APPROVE_PATH = {
-  storeDetail: (id: number) => `stores/manager/edit/${id}`,
-  driverDetail: (id: number) => `drivers/manager/edit/${id}`,
+  storeDetail: (id: number) => `stores/edit/${id}`,
+  driverDetail: (id: number) => `drivers/edit/${id}`,
   requestProduct: (id: number) => `requests/products/${id}`,
   requestDriver: (id: number) => `requests/drivers/${id}`,
   requestStore: (id: number) => `requests/stores/${id}`,
