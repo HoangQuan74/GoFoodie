3655ae1ae2cc9e297cf56ec3556ebc4b6b5ea34d
HoangQuan74 <quan.h.nguyen@tgl-sol.com>
2025-02-13 12:02:38 +0700

    feat: alter order table

--- src/modules/drivers/income/income.controller.ts
+++ src/modules/drivers/income/income.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { IncomeService } from './income.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CurrentUser } from 'src/common/decorators';
 import { JwtPayload } from 'src/common/interfaces';
 import { QueryIncomeDto } from './dto/query-income.dto';
 import { AuthGuard } from '../auth/auth.guard';
+import { EXCEPTIONS } from 'src/common/constants';
 
 @ApiTags('Income')
 @Controller('income')
@@ -20,7 +21,60 @@ export class IncomeController {
     const { id: driverId } = user;
     const queryBuilder = this.incomeService.createQueryBuilder('order')
       .where('order.driverId = :driverId', { driverId })
-      .andWhere('order.createdAt BETWEEN :startDate AND :endDate', { startDate, endDate });
-    return true;
+      .andWhere('order.createdAt BETWEEN :startDate AND :endDate', { startDate, endDate })
+      .select('SUM(order.deliveryFee)', 'deliveryFee')
+      .addSelect('SUM(order.tip)', 'tip')
+      .addSelect('SUM(order.peakHourFee)', 'peakHourFee')
+      .addSelect('SUM(order.parkingFee)', 'parkingFee')
+      .addSelect('COUNT(order.id)', 'totalOrder')
+    const result = queryBuilder.getRawOne();
+    return result;
+  }
+
+  @Get('/orders')
+  @ApiOperation({ summary: 'Get list order for the driver' })
+  @ApiResponse({ status: 200, description: 'Returns income' })
+  getIncomeOrders(@Query() query: QueryIncomeDto, @CurrentUser() user: JwtPayload) {
+    const { startDate, endDate } = query
+    const { id: driverId } = user;
+    const queryBuilder = this.incomeService.createQueryBuilder('order')
+      .where('order.driverId = :driverId', { driverId })
+      .andWhere('order.createdAt BETWEEN :startDate AND :endDate', { startDate, endDate })
+      .select([
+        'order.id',
+        'order.orderCode',
+        'order.deliveryFee',
+        'order.tip',
+        'order.peakHourFee',
+        'order.parkingFee',
+      ])
+    const result = queryBuilder.getMany();
+    return result;
+  }
+
+  @Get('/oredrs/:orderId')
+  @ApiOperation({ summary: 'Get order detail for the driver' })
+  @ApiResponse({ status: 200, description: 'Returns income' })
+  async getIncomeOrderDetails(@Param('orderId') orderId: number, @CurrentUser() user: JwtPayload) {
+    const { id: driverId } = user;
+    const queryBuilder = this.incomeService.createQueryBuilder('order')
+      .where('order.driverId = :driverId', { driverId })
+      .andWhere('order.id = :orderId', { orderId })
+      .select([
+        'order.id',
+        'order.orderCode',
+        'order.deliveryFee',
+        'order.tip',
+        'order.peakHourFee',
+        'order.parkingFee',
+        'order.transactionFee',
+        'order.appFee',
+      ])
+    const result = await queryBuilder.getOne();
+
+    if (!result)
+      throw new BadRequestException(EXCEPTIONS.NOT_FOUND)
+
+    return result;
   }
 }
