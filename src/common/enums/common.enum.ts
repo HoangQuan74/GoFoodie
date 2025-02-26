e4dbdf81da8eb6110ed5ca379f3cc96466002caf
HoangQuan74 <quan.h.nguyen@tgl-sol.com>
2025-02-26 11:19:46 +0700

    fix: merchant - enhance api dashboard

--- src/common/enums/common.enum.ts
+++ src/common/enums/common.enum.ts
@@ -3,3 +3,10 @@ export enum TimeRange {
   WEEK = 'week',
   MONTH = 'month',
 }
+
+export enum TimeRangeV2 {
+  TODAY = 'today',
+  YESTERDAY = 'yesterday',
+  LAST_7_DAYS = 'last_7_days',
+  LAST_30_DAYS = 'last_30_days',
+}
