8aa98cc6eef4431dca6d5e51a69a7582a67ad15c
hoangnt <hoang.t.nguyen02@tgl-sol.com>
2025-03-07 12:32:35 +0700

    fix: validate voucher

--- src/modules/merchant/vouchers/dto/create-voucher.dto.ts
+++ src/modules/merchant/vouchers/dto/create-voucher.dto.ts
@@ -5,6 +5,7 @@ import {
   IsBoolean,
   IsDate,
   IsEnum,
+  IsInt,
   IsNotEmpty,
   IsNumber,
   IsOptional,
@@ -13,11 +14,43 @@ import {
   Matches,
   MaxLength,
   Min,
+  Validate,
   ValidateNested,
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
 } from 'class-validator';
 import { EXCEPTIONS } from 'src/common/constants';
 import { ERefundType, EDiscountType, EVoucherType } from 'src/common/enums/voucher.enum';
 import { IdDto } from 'src/common/query';
+
+@ValidatorConstraint({ name: 'DiscountValueConstraint', async: false })
+class DiscountValueConstraint implements ValidatorConstraintInterface {
+  validate(value: number, args: ValidationArguments) {
+    const object = args.object as any;
+    return object.discountType !== EDiscountType.Percentage || value < 100;
+  }
+
+  defaultMessage() {
+    return 'Discount value must be less than 100%';
+  }
+}
+
+@ValidatorConstraint({ name: 'MaxDiscountValueConstraint', async: false })
+class MaxDiscountValueConstraint implements ValidatorConstraintInterface {
+  validate(value: number, args: ValidationArguments) {
+    const object = args.object as any;
+    return (
+      object.discountType !== EDiscountType.Percentage ||
+      value >= object.minOrderValue * object.discountValue ||
+      !object.minOrderValue
+    );
+  }
+
+  defaultMessage() {
+    return 'Max discount value must be greater than or equal to min order value * discount value';
+  }
+}
 
 export class CreateVoucherDto {
   @ApiProperty()
@@ -64,16 +97,19 @@ export class CreateVoucherDto {
   discountType: EDiscountType;
 
   @ApiProperty()
-  @IsNumber()
+  @IsInt()
   @Min(1)
+  @Validate(DiscountValueConstraint)
   discountValue: number;
 
   @ApiProperty({ description: 'Min order value to use voucher' })
   @IsNumber()
+  @Min(0)
   minOrderValue: number;
 
   @ApiProperty({ description: 'Max discount value of voucher' })
   @IsNumber()
+  @Validate(MaxDiscountValueConstraint)
   @IsOptional()
   maxDiscountValue: number;
 
